6e18638711e66a7b1ce210dc9295c3085ca5c1b6
Juan-GF-12 <juan.garrido62@inacapmail.cl>
2024-12-17 17:34:07 -0300

    update
    1

--- Proyecto/Evaluacion 2/serper_dev.py
+++ Proyecto/Evaluacion 2/serper_dev.py
@@ -1,15 +1,15 @@
 import http.client
 import json
-
+busqueda=input("¿Que deseas buscar hoy?: ")
 conn = http.client.HTTPSConnection("google.serper.dev")
-payload = json.dumps({
-  "q": "apple inc"
+info_busqueda = json.dumps({
+  "q": busqueda
 })
 headers = {
   'X-API-KEY': '4f044cd471e12a1204ee4d3e06e7b6f12bf57d40',
   'Content-Type': 'application/json'
 }
-conn.request("POST", "/search", payload, headers)
+conn.request("POST", "/search", info_busqueda, headers)
 res = conn.getresponse()
 data = res.read()
 print(data.decode("utf-8"))
